231464e556b46d4649857c7dd532d5d03886e03e
Pavel Vodopyan <pavel.vodopyan@gmail.com>
2015-03-28 21:17:51 +0000

    Add very basic sceleton of application

---
+++ app.js
@@ -0,0 +1,65 @@
+
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+
+var routes = require('./lib/route/index');
+
+var app = express();
+
+// view engine setup
+var handlebars = require('express-handlebars')
+    .create({
+        defaultLayout: 'main',
+        extname: '.hbs',
+    });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// custom routes
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
