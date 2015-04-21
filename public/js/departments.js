6239eb99e9f541c7107a48563f99a649619e6635
Pavel Vodopyan <pavel.vodopyan@gmail.com>
2015-04-21 08:22:11 +0100

    Removing departments

--- public/js/departments.js
+++ public/js/departments.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
     $('#add_new_department_btn').on('click', function(e){
         $('#add_new_department_row').removeClass('hidden');
     });
+
+    $('button.close').on('click', function(e){
+        var delete_form = $('#delete_form');
+        delete_form.attr('action', delete_form.attr('action') + $(this).attr('value') + '/');
+        return delete_form.submit();
+    });
 });
