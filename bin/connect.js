07db2d08fadf605c6f0b7e49c7f47ae7b0b21a40
Reekomer <jeremy.degail@gmail.com>
2017-03-18 18:37:09 +0100

    Update connect.js

--- bin/connect.js
+++ bin/connect.js
@@ -1,4 +1,5 @@
-var connectionString = "postgres://*USERNAME*:*PASSWORD*@*HOST*:*PORT:/*DATABASE*"
+var connectionString = "postgres://*eyjqdcmvieocxx*:*fae2cba82e050acf3c228dbd543966f5a2e1e9806c655dd22dfab7b5fbd24328*@*ec2-54-75-231-195.eu-west-1.compute.amazonaws.com
+*:*5432:/*da8bc6tso4apb*"
 
 pg.connect(connectionString, function(err, client, done) {
    client.query('SELECT * FROM your_table', function(err, result) {
