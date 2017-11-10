cbc059d5172a95dc8bb1b3b665234514ecfad711
Pavlo Vodopyan <pavel.vodopyan@gmail.com>
2017-11-10 07:28:18 +0000

    Finish happy path test for deducted column on team view.

--- t/integration/team_view/deducted_column.js
+++ t/integration/team_view/deducted_column.js
@@ -33,14 +33,13 @@
  *    * create user B
  *    * create new bank holiday to be on 2 Aug 2016
  *    * as user B create a holiday request from 28 July to morning of 4 Aug 2016 (a)
- *
  *    * as user B create a holiday request from 12 Aug to 15 Aug 2016 (b)
  *    * as user B create a holiday request from 26 Aug to 2 Sep 2016 (d)
  *    * as user B create a sick day request from 18 to 18 Aug 2016 (e)
  *    * as user A approve leaves (a), (b), (d), and (e)
  *    * as user B cretae a holiday request from 24 Aug to 24 Aug 2016 (c)
- *    * navigate to team view and ensure that it shows 9.5 days were deducted for Aug 2016
- *    * 2 days deducted for July 2016
+ *    * navigate to team view and ensure that it shows 9 days were deducted for Aug 2016
+ *    * 1.5 days deducted for July 2016
  *    * 2 days deducted for Sept 2016
  *
  * */
@@ -189,8 +188,200 @@ describe('Case when holidays spans through more then one month and is devided by
     .then(function(){done()});
   });
 
+  it("Open Book leave popup window", function(done){
+    driver.findElement(By.css('#book_time_off_btn'))
+      .then( el => el.click() )
+      // This is very important line when working with Bootstrap modals!
+      .then( el => driver.sleep(1000) )
+      .then(() => done());
+  });
+
+  it('As user B create a holiday request from 12 Aug to 15 Aug 2016 (b)', function(done){
+    submit_form_func({
+      driver      : driver,
+      form_params : [{
+        selector : 'input#from',
+        value    : '2016-08-12',
+      },{
+        selector : 'input#to',
+        value    : '2016-08-15',
+      }],
+      message : /New leave request was added/,
+    })
+    .then(function(){done()});
+  });
+
+  it("Open Book leave popup window", function(done){
+    driver.findElement(By.css('#book_time_off_btn'))
+      .then( el => el.click() )
+      // This is very important line when working with Bootstrap modals!
+      .then( el => driver.sleep(1000) )
+      .then(() => done());
+  });
+
+  it('As user B create a holiday request from 26 Aug to 2 Sep 2016', function(done){
+    submit_form_func({
+      driver      : driver,
+      form_params : [{
+        selector : 'input#from',
+        value    : '2016-08-26',
+      },{
+        selector : 'input#to',
+        value    : '2016-09-02',
+      }],
+      message : /New leave request was added/,
+    })
+    .then(() => done());
+  });
+
+  it("Open Book leave popup window", function(done){
+    driver.findElement(By.css('#book_time_off_btn'))
+      .then( el => el.click() )
+      // This is very important line when working with Bootstrap modals!
+      .then( el => driver.sleep(1000) )
+      .then(() => done());
+  });
+
+  it('As user B create a sick day request from 18 to 18 Aug 2016', function(done){
+    submit_form_func({
+      driver      : driver,
+      form_params : [{
+        selector : 'select[name="leave_type"]',
+        option_selector : 'option:nth-child(2)',
+      },{
+        selector : 'input#from',
+        value    : '2016-08-18',
+      },{
+        selector : 'input#to',
+        value    : '2016-08-18',
+      }],
+      message : /New leave request was added/,
+    })
+    .then(() => done());
+  });
+
+  it("Logout from user B", function(done){
+    logout_user_func({
+      application_host : application_host,
+      driver           : driver,
+    })
+    .then(() => done());
+  });
+
+  it("Login as user A (admin)", function(done){
+    login_user_func({
+      application_host : application_host,
+      user_email       : email_A,
+      driver           : driver,
+    })
+    .then(() => done());
+  });
+
+  it("Open requests page", function( done ){
+    open_page_func({
+      url    : application_host + 'requests/',
+      driver : driver,
+    })
+    .then(() => done());
+  });
+
+  it("Approve newly added leave request", function(done){
+    let click_selector = `tr[vpp="pending_for__${email_B}"] .btn-success`;
+    driver
+      .findElement(By.css( click_selector ))
+      .then(el => el.click())
+      .then(() => driver.findElement(By.css( click_selector )))
+      .then(el => el.click())
+      .then(() => driver.findElement(By.css( click_selector )))
+      .then(el => el.click())
+      .then(() => driver.findElement(By.css( click_selector )))
+      .then(el => el.click())
+      .then(() => done());
+  });
+
+  it("Logout from user A (admin)", function(done){
+    logout_user_func({
+      application_host : application_host,
+      driver           : driver,
+    })
+    .then(() => done());
+  });
+
+  it("Login as user B", function(done){
+    login_user_func({
+      application_host : application_host,
+      user_email       : email_B,
+      driver           : driver,
+    })
+    .then(() => done());
+  });
+
+  it("Open Book leave popup window", function(done){
+    driver.findElement(By.css('#book_time_off_btn'))
+      .then( el => el.click() )
+      // This is very important line when working with Bootstrap modals!
+      .then( el => driver.sleep(1000) )
+      .then(() => done());
+  });
+
+  it('As user B cretae a holiday request from 24 Aug to 24 Aug 2016', function(done){
+    submit_form_func({
+      driver      : driver,
+      form_params : [{
+        selector : 'input#from',
+        value    : '2016-08-24',
+      },{
+        selector : 'input#to',
+        value    : '2016-08-24',
+      }],
+      message : /New leave request was added/,
+    })
+    .then(function(){done()});
+  });
+
+  it('Navigate to team view and ensure that it shows 9 days were deducted for Aug 2016', function(done){
+    open_page_func({
+      url    : application_host + 'calendar/teamview/?date=2016-08',
+      driver : driver,
+    })
+    .then(() => driver.findElement(By.css(`tr[data-vpp-user-list-row="${user_id_B}"] td.teamview-deducted-days`)))
+    .then(el => el.getText())
+    .then(txt => {
+      expect(txt, 'Ensure that system shows 9 days as deducted')
+        .to.be.eql('9');
+      done();
+    });
+  });
+
+  it('1.5 days deducted for July 2016', function(done){
+    open_page_func({
+      url    : application_host + 'calendar/teamview/?date=2016-07',
+      driver : driver,
+    })
+    .then(() => driver.findElement(By.css(`tr[data-vpp-user-list-row="${user_id_B}"] td.teamview-deducted-days`)))
+    .then(el => el.getText())
+    .then(txt => {
+      expect(txt, 'Ensure that system shows 1.5 days as deducted')
+        .to.be.eql('1.5');
+      done();
+    });
+  });
+
+  it('2 days deducted for Sept 2016', function(done){
+    open_page_func({
+      url    : application_host + 'calendar/teamview/?date=2016-09',
+      driver : driver,
+    })
+    .then(() => driver.findElement(By.css(`tr[data-vpp-user-list-row="${user_id_B}"] td.teamview-deducted-days`)))
+    .then(el => el.getText())
+    .then(txt => {
+      expect(txt, 'Ensure that system shows 2 days as deducted')
+        .to.be.eql('2');
+      done();
+    });
+  });
+
   after(function(done){
-    console.log('>>> ' + email_B);
     driver.quit().then(() => done());
   });
 });
