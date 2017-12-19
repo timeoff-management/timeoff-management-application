384964f118c457f4f05fdcc2a805dfb0dda85282
Pavlo Vodopyan <pavel.vodopyan@gmail.com>
2017-12-19 15:15:47 +0000

    Add table to track the allowance adjustment per year

---
+++ migrations/20171219-allowance-adjustment-per-year.js
@@ -0,0 +1,36 @@
+
+'use strict';
+
+var models = require('../lib/model/db'),
+  Promise = require('bluebird');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    queryInterface.describeTable('Users')
+      .then(function(attributes){
+
+        if ( ! attributes.hasOwnProperty('adjustment')) {
+          return 1;
+        }
+
+        let sql = 'INSERT INTO user_allowance_adjustment (year, adjustment, user_id, created_at) '
+          + 'SELECT 2017 AS year, adjustment as adjustment, id as user_id, date() || \' \' || time() as created_at '
+          + 'FROM users';
+
+        console.log(sql);
+
+        return queryInterface.sequelize.query( sql );
+      })
+
+      // TODO remove the users.adjustment column
+
+      .then(() => Promise.resolve());
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+    // No way back!
+    return Promise.resolve();
+  }
+};
