71dc373bd8ba43212efa67f191434ddfb734d370
Pavlo Vodopyan <pavel.vodopyan@gmail.com>
2017-12-28 16:00:12 +0000

    Add timezone field to company and UI to edit it

---
+++ migrations/20171228-add-time-zone-field-to-company.js
@@ -0,0 +1,28 @@
+
+'use strict';
+
+var models = require('../lib/model/db');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return queryInterface.describeTable('Companies')
+      .then(function(attributes){
+
+        if (attributes.hasOwnProperty('timezone')) {
+          return 1;
+        }
+
+        return queryInterface.addColumn(
+          'Companies',
+          'timezone',
+          models.Company.attributes.timezone
+        );
+      });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface
+      .removeColumn('Companies', 'timezone');
+  }
+};
