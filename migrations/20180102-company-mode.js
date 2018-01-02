43153ad4e0016323d2258c5ade20c7f3a4709d80
Pavlo Vodopyan <pavel.vodopyan@gmail.com>
2018-01-02 20:14:03 +0000

    Add missing migration

---
+++ migrations/20180102-company-mode.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+var models = require('../lib/model/db');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    queryInterface.describeTable('Companies').then(function(attributes){
+
+      if (attributes.hasOwnProperty('mode')) {
+        return 1;
+      }
+
+      return queryInterface.addColumn(
+        'Companies',
+        'mode',
+        models.Company.attributes.mode
+      );
+    });
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.removeColumn('Companies', 'mode');
+  }
+};
