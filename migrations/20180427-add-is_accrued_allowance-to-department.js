6d94929ba73be2b8a9d38b898d376cf643bb9f8b
Pavlo Vodopyan <pavel.vodopyan@gmail.com>
2018-04-27 14:49:28 +0100

    Add migration that adds new column to department table

---
+++ migrations/20180427-add-is_accrued_allowance-to-department.js
@@ -0,0 +1,28 @@
+
+'use strict';
+
+var models = require('../lib/model/db');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return queryInterface.describeTable('Departments')
+      .then(function(attributes){
+
+        if (attributes.hasOwnProperty('is_accrued_allowance')) {
+          return 1;
+        }
+
+        return queryInterface.addColumn(
+          'Departments',
+          'is_accrued_allowance',
+          models.Department.attributes.is_accrued_allowance
+        );
+      });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface
+      .removeColumn('Departments', 'is_accrued_allowance');
+  }
+};
