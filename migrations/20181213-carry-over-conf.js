5e38d6d297967082f95f2406b234061ca792d3ee
Pavlo Vodopyan <Pavlo@pavlo-macbookpro.local>
2018-12-27 09:53:09 +0000

    Add carry over UI

---
+++ migrations/20181213-carry-over-conf.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+const models = require('../lib/model/db');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    queryInterface.describeTable('Companies').then((attributes) => {
+
+      if (attributes.hasOwnProperty('carry_over')) {
+        return 1;
+      }
+
+      return queryInterface.addColumn(
+        'Companies',
+        'carry_over',
+        models.Company.attributes.carry_over
+      );
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.removeColumn('Companies', 'carry_over');
+  }
+};
