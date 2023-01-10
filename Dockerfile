f48c23c86ed3f88a1d450e427634d282defab3da
Sergio Corona <secorona.rodriguez@gmail.com>
2023-01-09 22:16:18 -0600

    FIX: Fix build issues for the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,7 @@
 FROM alpine:latest as dependencies
 
 RUN apk add --no-cache \
-    nodejs npm 
+    nodejs npm build-base python3 sqlite make gcc musl-dev libc-dev 
 
 COPY package.json  .
 RUN npm install 
--- migrations/20191030-compress-email-audit.js
+++ migrations/20191030-compress-email-audit.js
@@ -2,14 +2,14 @@
 'use strict';
 
 const
-  htmlToText = require('html-to-text'),
+  { htmlToText } = require('html-to-text'),
   Promise = require('bluebird'),
   models = require('../lib/model/db');
 
 module.exports = {
   up: () => {
     return models.EmailAudit.findAll()
-      .map(rec => rec.update({body : htmlToText.fromString(rec.body)}), {concurrency: 1})
+      .map(rec => rec.update({body : htmlToText(rec.body)}), {concurrency: 1})
       .then(() => console.log('Done!'));
   },
 
