5beb5247f874ea55b59c04d338f6f50d2fa80c04
Timothé Mermet-Buffet <tim@aliengen.com>
2023-10-17 09:18:05 +0800

    Prettier.

--- public/js/bank_holidays.js
+++ public/js/bank_holidays.js
@@ -1,22 +1,22 @@
-
 $(document).ready(function () {
-  $('button.bankholiday-remove-btn').on('click', function(e){
-
+  $("button.bankholiday-remove-btn").on("click", function (e) {
     e.stopPropagation();
 
-    var delete_form = $('#delete_bankholiday_form');
-    delete_form.attr('action', delete_form.attr('action') + $(this).attr('value') + '/');
+    var delete_form = $("#delete_bankholiday_form");
+    delete_form.attr(
+      "action",
+      delete_form.attr("action") + $(this).attr("value") + "/"
+    );
 
     delete_form.submit();
 
     return false;
   });
 
-  $('#bankholiday-import-btn').on('click', function(e){
-
+  $("#bankholiday-import-btn").on("click", function (e) {
     e.stopPropagation();
 
-    var import_form = $('#import_bankholiday_form');
+    var import_form = $("#import_bankholiday_form");
 
     import_form.submit();
 
--- public/js/settings_general.js
+++ public/js/settings_general.js
@@ -1,11 +1,12 @@
-
 $(document).ready(function () {
-  $('button.leavetype-remove-btn').on('click', function(e){
-
+  $("button.leavetype-remove-btn").on("click", function (e) {
     e.stopPropagation();
 
-    var delete_form = $('#delete_leavetype_form');
-    delete_form.attr('action', delete_form.attr('action') + $(this).attr('value') + '/');
+    var delete_form = $("#delete_leavetype_form");
+    delete_form.attr(
+      "action",
+      delete_form.attr("action") + $(this).attr("value") + "/"
+    );
 
     delete_form.submit();
 
