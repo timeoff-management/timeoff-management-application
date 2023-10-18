bb33c960c0d382c58b9c1749b94b5583594b4944
Timothé Mermet-Buffet <tim@aliengen.com>
2023-10-18 19:16:57 +0800

    Fix.

--- t/lib/open_page.js
+++ t/lib/open_page.js
@@ -1,18 +1,18 @@
 'use strict'
 
-var webdriver = require('selenium-webdriver'),
-  Promise = require('bluebird')
+const webdriver = require('selenium-webdriver')
+const Promise = require('bluebird')
 
-module.exports = Promise.promisify(function(args, callback) {
-  var url = args.url,
-    driver = args.driver,
-    result_callback = callback
+module.exports = Promise.promisify((args, callback) => {
+  const url = args.url
+  const driver = args.driver
+  const result_callback = callback
 
   // Open front page
-  driver.get(url).then(function() {
+  driver.get(url).then(() => {
     // "export" current driver
     result_callback(null, {
-      driver: driver
+      driver
     })
   })
 })
